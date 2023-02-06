e939c91acd986b50a520d7d8e64fb80797cadc29
MirkusX <marcusk.kodehode@gmail.com>
2023-02-06 12:27:24 +0100

    feat/choose horse, winner declare

--- src/Pages/Frontpage.js
+++ src/Pages/Frontpage.js
@@ -1,8 +1,37 @@
+import { useState } from "react";
+import { HorseArray } from "./FrontpageFiles.js/HorseArray";
+
 export const Frontpage = () => {
+  const [winner, setWinner] = useState();
+  const [bet, setBet] = useState([]);
+  const choose = (item) => {
+    setBet([{ horse: item }]);
+    console.log(bet);
+  };
+  const start = () => {
+    setWinner(HorseArray[Math.floor(Math.random() * HorseArray.length)]);
+    bet.forEach((item, index) => {
+      if (item.horse === winner) {
+        console.log("winner");
+      }
+    });
+  };
   return (
     <section>
       <div>
-        <h1>frontpage</h1>
+        <h1>{winner}</h1>
+      </div>
+      <div>
+        {HorseArray.map((item, index) => {
+          return (
+            <button key={index} onClick={() => choose(item)}>
+              {item}
+            </button>
+          );
+        })}
+      </div>
+      <div>
+        <button onClick={() => start()}>start</button>
       </div>
     </section>
   );
---
+++ src/Pages/FrontpageFiles.js/HorseArray.js
@@ -0,0 +1,1 @@
+export const HorseArray = ["Red", "Blue", "Green", "White", "Pink"];
